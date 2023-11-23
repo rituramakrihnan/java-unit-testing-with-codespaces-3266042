e0a3d7c604b173753fa05614446ae1df9fa472ef
Ritu <123215253+rituramakrihnan@users.noreply.github.com>
2023-11-23 03:58:39 +0000

    excercise 3 code commit

---
+++ src/main/java/HairSalon.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class HairSalon {
+
+  private String[] openingDays = { "Monday", "Tuesday" };
+
+  public String[] getOpeningDays() {
+    return new String[] {};
+  }
+  public boolean isOpen(String weekDay) {
+    return false;
+  }
+  
+}
---
+++ src/test/java/HairSalonTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+public class HairSalonTest {
+  HairSalon hairSalon = new HairSalon();
+
+    @Test
+    public void testGetOpeningDays() {
+
+    }
+
+    @Test
+    public void testSalonIsOpenOnMonday() {
+
+    }
+
+    @Test
+    public void testSalonIsClosedOnWednesday() {
+
+    }
+
+    @Test
+    public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
+
+    }
+}
