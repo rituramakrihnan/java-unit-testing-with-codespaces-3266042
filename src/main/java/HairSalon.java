7878e3af7bd26f3a92847483c740284a2cba02fc
Ritu <123215253+rituramakrihnan@users.noreply.github.com>
2023-11-23 04:36:25 +0000

    excercise 4 code commit

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -2,21 +2,42 @@ import java.util.Arrays;
 
 public class HairSalon {
 
-  private String[] openingDays = { "Monday", "Tuesday" };
+    private int haircutPrice = 30;
+    private Calculator calculator = new Calculator();
 
-  public String[] getOpeningDays() {
-    return openingDays;
-  }
+    enum Day {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
 
-  public boolean isOpen(String weekDay) {
-    if (!weekDay.endsWith("day")) {
-      throw new IllegalArgumentException("The weekday argument must end in the letters 'day'");
+    private Day[] openingDays = {Day.MONDAY, Day.TUESDAY};
+
+    // This method should return the openingDays array.
+    public Day[] getOpeningDays() {
+        return openingDays;
     }
-    if (Arrays.asList(openingDays).contains(weekDay)) {
-      return true;
-    } else {
-      return false;
+
+    // This method should return true if the weekday passed in is present in the openingDays array.
+    // If not it should return false.
+    public boolean isOpen(Day weekDay) {
+        if(Arrays.asList(openingDays).contains(weekDay)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
-  
+
+    public int getHaircutPrice() {
+        return haircutPrice;
+    }
+
+    public int applyDiscount() {
+        return calculator.divide(haircutPrice, 2) + 5;
+    }
+    
 }
--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class HairSalonTest {
 
@@ -8,30 +11,38 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
-        String[] expected = new String[]{"Monday", "Tuesday"};
+        HairSalon.Day[] expected = new HairSalon.Day[]{HairSalon.Day.MONDAY, HairSalon.Day.TUESDAY};
 
-        String[] actual = hairSalon.getOpeningDays();
+        HairSalon.Day[] actual = hairSalon.getOpeningDays();
 
         assertArrayEquals(expected, actual);
     }
+    
+    @ParameterizedTest
+    @EnumSource(value = HairSalon.Day.class, names = {"MONDAY", "TUESDAY"})
+    public void testSalonIsOpen(HairSalon.Day day) {
+        assertTrue(hairSalon.isOpen(day));
+    }
 
-    @Test
-    public void testSalonIsOpenOnMonday() {
-        assertTrue(hairSalon.isOpen("Monday"));
+    @ParameterizedTest
+    @EnumSource(value = HairSalon.Day.class, names = {"WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"})
+    public void testSalonIsClosed(HairSalon.Day day) {
+        assertFalse(hairSalon.isOpen(day));
     }
 
     @Test
-    public void testSalonIsClosedOnWednesday() {
-        assertFalse(hairSalon.isOpen("Wednesday"));
+    public void testNull() {
+        assertFalse(hairSalon.isOpen(null));
     }
 
     @Test
-    public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen("Hello"));
+    public void testHalfPriceDiscount() {
+        int expected = 20;
 
-        String expected = "The weekday argument must end in the letters 'day'";
+        int actual = hairSalon.applyDiscount();
 
-        String actual = exception.getMessage();
+        Calculator calculator = new Calculator();
+        assumeTrue(calculator.divide(hairSalon.getHaircutPrice(), 2) == 15);
 
         assertEquals(expected, actual);
     }
