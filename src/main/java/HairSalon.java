3ad0b9421b7aba45917a5bd205dd4739bd6b7f99
Ritu <123215253+rituramakrihnan@users.noreply.github.com>
2023-11-23 04:22:34 +0000

    excercise 3 challenge code commit

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -5,10 +5,18 @@ public class HairSalon {
   private String[] openingDays = { "Monday", "Tuesday" };
 
   public String[] getOpeningDays() {
-    return new String[] {};
+    return openingDays;
   }
+
   public boolean isOpen(String weekDay) {
-    return false;
+    if (!weekDay.endsWith("day")) {
+      throw new IllegalArgumentException("The weekday argument must end in the letters 'day'");
+    }
+    if (Arrays.asList(openingDays).contains(weekDay)) {
+      return true;
+    } else {
+      return false;
+    }
   }
   
 }
